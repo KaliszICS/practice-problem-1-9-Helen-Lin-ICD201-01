c09360ab201cc6be96ec8849e6468495a0157db2
Helen-Lin-ICD201-01 <helen.lin@student.tdsb.on.ca>
2024-09-30 14:10:12 +0000

    jungkook

--- PP1_9.py
+++ PP1_9.py
@@ -1,24 +1,34 @@
-
+'''
+  Lesson: String Manipulation
+  Name: Helen Lin
+  Date Created: September 30, 2024
+  Date Last Modified: September 30, 2024
+'''
 
 def q1(): 
   #Write Assignment code here
-
+  print('"Hello World"')
 
 def q2(): 
   #Write Assignment code here
-
+  word = input("Input a word: ")
+  print(word.lower())
+  print(word.upper())
 
 def q3(): 
   #Write Assignment code here
-
+  word = input("Input a word at least 5 letters long: ")
+  print(word[1:5])
 
 def q4(): 
   #Write Assignment code here
-
+  word = input("Input a word: ")
+  print(word.index("o"))
 
 def q5(): 
   #Write Assignment code here
-
+  word = input("Input a word: ")
+  print(len(word))
 
 
 #Do not alter the following code
